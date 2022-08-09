a886b33610de781546d3ea46ba09a63b5d7bacfd
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-09 14:53:27 +0200

    Play around with math operators and functions

--- exercise.py
+++ exercise.py
@@ -24,7 +24,32 @@ print(round(e, 1))
 f = 17 % 5
 print(f)
 
+g = 4 + 5j
+
+print(e*g)
+
 zero = 0
 print(bool(zero))
 
 # print(b/zero)
+
+# ---- Task 2 - basic math functions ----#
+import math
+
+print(max(76.0, 12.0, -0.5))
+print(min(2305, 220, 755.0))
+print(abs(-67.63))
+print(pow(2, 8))
+print(math.ceil(75/7))
+print(math.floor(75/7))
+print(max(True, False))
+
+print(max(1245, 3789, 1436))
+print(min(1245, 3789, 1436))
+print(abs(-6892.45))
+print(pow(5, 4))
+print(math.ceil(875/6))
+print(math.floor(875/6))
+
+
+
