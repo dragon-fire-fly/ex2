4f9f4f9e9f52125728a10248a8fdc8b299dcafe4
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-09 14:43:46 +0200

    Complete basic operators task

---
+++ exercise.py
@@ -0,0 +1,30 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+#---- Task 1 - Basic Operators - ----#
+
+a = 123 + 456
+print(a)
+
+b = 456 - 123
+print(b)
+print(bool(b))
+
+c = a * b
+d = c / a
+
+print(c)
+print(d)
+
+e = 65/8
+print(e)
+print(round(e, 1))
+
+f = 17 % 5
+print(f)
+
+zero = 0
+print(bool(zero))
+
+# print(b/zero)
